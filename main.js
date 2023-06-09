0ee348d25d4dd59435a8ebb2e275ace648a17330
luciano <lucianorgentile@gmail.com>
2023-06-08 21:49:07 -0300

    Proyecto final

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+Swal.fire({
+    title: "¿Eres mayor de edad?",
+    showConfirmButton: true,
+    confirmButtonText: 'Soy Mayor',
+    showCancelButton: true,
+    cancelButtonText: 'No soy mayor',
+    footer: '<span>Información Importante</span>',
+})
 let miFormulario = document.getElementById("formulario");
 miFormulario.addEventListener("submit", validarFormulario);
 function validarFormulario(e) {
@@ -37,3 +45,25 @@ function validarFormulario(e) {
 
     console.log ("El valor de su cuota es: " + (valorCuota(interesAcumulado)))
 }
+const urlUsuarios = "https://jsonplaceholder.typicode.com/users";
+const listaUsuarios = document.querySelector('#lista-usuarios')
+fetch(urlUsuarios)
+    .then((response) => response.json())
+    .then((data) => {
+        data.forEach((usuario) => {
+            const li = document.createElement('li');
+            li.textContent = usuario.name + " Web: " + usuario.website;
+            listaUsuarios.appendChild(li);
+        })      
+        });
+const nombre = ['Roman', 'Jose', 'Pedro', 'Nacho', 'Luana', 'Silvia', 'Paula']
+const cuotas = ['6 cuotas', '9 cuotas', '12 cuotas', '18 cuotas', '24 cuotas']
+
+setInterval(() => {
+    Toastify({
+        text: `${nombre[Math.floor((Math.random()*7))]} consulto en ${cuotas[Math.floor((Math.random()*5))]}`,
+        gravity: 'bottom',
+        duration: 2000
+    }).showToast();
+}, 2000)
+    
